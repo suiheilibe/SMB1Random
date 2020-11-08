138cf9a59762b80d42c49189877c1576bad29352
suiheilibe <ntjapanize@yahoo.co.jp>
2020-11-08 10:58:19 +0900

    bit perfect color calculation test

--- SMB35StagePicker.js
+++ SMB35StagePicker.js
@@ -1,6 +1,6 @@
 const NUM_WORLD = 8;
 const NUM_AREA  = 4;
-const COLOR_STAGE = 0xcc000000;
+const COLOR_STAGE = 0xcc0000ff;
 
 const container = document.querySelector("div.container");
 //console.log(container);
@@ -34,7 +34,7 @@ for (let i = 1; i <= NUM_WORLD; i++) {
 function updateAlpha(stage, alpha) {
   const e = stage.element;
   const curColor = stage.color;
-  const color = curColor;
+  const color = curColor - (curColor & 0xff) + Math.floor(0xff * alpha);
   e.style.backgroundColor = '#' + color.toString(16);
 }
 
